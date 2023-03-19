e93c1dbffb01b4e864d62e0994069c190d8ff558
Luis Carlos Pacheco Lanzziano <pachecolanzziano@gmail.com>
2023-03-18 23:06:06 -0500

    CRUD
    CRUD

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -3,18 +3,35 @@ const router = express.Router();
 const userSchema = require("../model/usuario")
 
 //creaete usuario
-router.post('/usuarios', (req, res) => {
-    const usuario = userSchema(req.body);//retornara el usuario creado
-    usuario.save()
-        .then((data) => res.json(data))
-        .catch((error)=> res.json({message: error}))
-    
+router.get('/usuarios/:id', async(req, res) => {
+    const { id } = req.params.id;
+    const usuario = await userSchema.findById(id);
+    res.json(usuario);
 })
 
 router.get('/usuarios', async(req, res) => {
     const usuarios = await userSchema.find();
     res.json(usuarios);
-    
 })
+
+router.post('/usuarios', (req, res) => {
+    const usuario = userSchema(req.body);//retornara el usuario creado
+    usuario.save()
+        .then((data) => res.json(data))
+        .catch((error)=> res.json({message: error}))
+})
+
+router.put('/usuarios/:id', async(req, res) => {
+    await userSchema.findByIdAndUpdate(req.params.id, req.body)
+    .then((data) => res.json(data))
+    .catch((error)=> res.json({message: error}))
+})
+
+router.delete('/usuarios/:id', async(req, res) => {
+    await userSchema.findByIdAndRemove(req.params.id)
+    .then((data) => res.json(data))
+    .catch((error)=> res.json({message: error}))
+})
+
 
 module.exports = router;
